cb89c7c606ea2bb9aa4b7e3441e75b29d878e77c
Daniel <luisdanielcastro16@gmail.com>
2023-07-21 15:00:50 -0600

    Fin de sección 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { CounterModule } from './counter/components/counter/counter.module';
+import { heroeListModule } from './heroes/heroe-list.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    heroeListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
